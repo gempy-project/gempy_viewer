26744c4416793930aa815009ccf280389b448efb
MigueldelaVarga <miguel@terranigma-solutions.com>
2023-07-31 10:11:34 +0200

    - [WIP] Preparing plot2d with octrees

--- tests/test_plotting/test_plot_2d.py
+++ tests/test_plotting/test_plot_2d.py
@@ -1,9 +1,12 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import pytest
 
 import gempy as gp
 import gempy_viewer as gpv
+from gempy import GeoModel
 from gempy_viewer.modules.plot_2d.visualization_2d import Plot2D
+from tests.conftest import _one_fault_model_generator
 
 
 # TODO: - [x] Test sections
@@ -12,6 +15,12 @@ from gempy_viewer.modules.plot_2d.visualization_2d import Plot2D
 
 
 class TestPlot2DInputData:
+    @pytest.fixture(scope='class')
+    def one_fault_model_no_interp(self) -> GeoModel:
+        geo_data = _one_fault_model_generator()
+
+        return geo_data
+
     def test_plot_2d_data_default(self, one_fault_model_no_interp):
         _: Plot2D = gpv.plot_2d(one_fault_model_no_interp)
 
@@ -22,18 +31,18 @@ class TestPlot2DInputData:
         geo_model = one_fault_model_no_interp
         section_dict = {'section_SW-NE': ([250, 250], [1750, 1750], [100, 100]),
                         'section_NW-SE': ([250, 1750], [1750, 250], [100, 100])}
-        
+
         gp.set_section_grid(
             grid=geo_model.grid,
             section_dict=section_dict
         )
-        
+
         gpv.plot_2d(
             model=geo_model,
             section_names=['section_SW-NE', 'section_NW-SE'],
-            show_section_traces=False, # TODO: Test this one
+            show_section_traces=False,  # TODO: Test this one
         )
-    
+
     def test_plot_2d_topography(self, one_fault_model_no_interp):
         gp.set_topography_from_random(
             grid=one_fault_model_no_interp.grid,
@@ -41,35 +50,35 @@ class TestPlot2DInputData:
             d_z=np.array([600, 2000]),
             topography_resolution=np.array([60, 60])
         )
-        
+
         gpv.plot_2d(
             model=one_fault_model_no_interp,
             section_names=['topography'],
             show_topography=True,
-            show_section_traces=False, # TODO: Test this one
+            show_section_traces=False,  # TODO: Test this one
         )
-    
+
     def test_plot_2d_topography_and_sections(self, one_fault_model_no_interp):
         gp.set_section_grid(
             grid=one_fault_model_no_interp.grid,
             section_dict={'section_SW-NE': ([250, 250], [1750, 1750], [100, 100]),
-                            'section_NW-SE': ([250, 1750], [1750, 250], [100, 100])}
+                          'section_NW-SE': ([250, 1750], [1750, 250], [100, 100])}
         )
-        
+
         gp.set_topography_from_random(
             grid=one_fault_model_no_interp.grid,
             fractal_dimension=1.2,
             d_z=np.array([600, 2000]),
             topography_resolution=np.array([60, 60])
         )
-        
+
         gpv.plot_2d(
             model=one_fault_model_no_interp,
             section_names=['section_SW-NE', 'section_NW-SE', 'topography'],
             show_topography=True,
             show_section_traces=True  # TODO: Test this one
         )
-    
+
     def test_plot_2d_all_together(self, one_fault_model_no_interp):
         gp.set_section_grid(
             grid=one_fault_model_no_interp.grid,
@@ -91,21 +100,39 @@ class TestPlot2DInputData:
             show_topography=True,
             show_section_traces=True  # TODO: Test this one
         )
-        
-        
+
+
 class TestPlot2DSolutions:
+    @pytest.fixture(scope='class')
+    def one_fault_model_topo_solution(self) -> GeoModel:
+        one_fault_model = _one_fault_model_generator()
+
+        gp.set_section_grid(
+            grid=one_fault_model.grid,
+            section_dict={'section_SW-NE': ([250, 250], [1750, 1750], [100, 100]),
+                          'section_NW-SE': ([250, 1750], [1750, 250], [100, 100])}
+        )
+
+        gp.set_topography_from_random(
+            grid=one_fault_model.grid,
+            fractal_dimension=1.2,
+            d_z=np.array([600, 2000]),
+            topography_resolution=np.array([60, 60])
+        )
+
+        gp.compute_model(one_fault_model)
+        return one_fault_model
+
     def test_plot_2d_solutions_default(self, one_fault_model_topo_solution):
         print(one_fault_model_topo_solution.structural_frame)
         _: Plot2D = gpv.plot_2d(one_fault_model_topo_solution)
 
-
     def test_plot_2d_all_together(self, one_fault_model_topo_solution):
-
         gpv.plot_2d(
             model=one_fault_model_topo_solution,
             section_names=['section_SW-NE', 'section_NW-SE', 'topography'],
             direction=['x', 'y', 'y'], cell_number=['mid', 'mid', 'mid'],
-            show_lith=[False, False, False, True, True , True],
+            show_lith=[False, False, False, True, True, True],
             show_boundaries=[False, False, False, True, True, True],
             show_scalar=[False, False, False, False, True, True],
             series_n=[0, 0, 0, 0, 0, 1],
@@ -120,3 +147,32 @@ class TestPlot2DSolutions:
 
         plt.show()
 
+
+class TestPlot2DSolutionsOctrees:
+    @pytest.fixture(scope='class')
+    def one_fault_model_topo_solution_octrees(self) -> GeoModel:
+        one_fault_model = _one_fault_model_generator()
+        one_fault_model.grid.regular_grid.resolution = [2, 4, 2]
+
+        one_fault_model.interpolation_options.number_octree_levels = 4
+        
+        # TODO: Test octree regular grid with everything else combined
+        # gp.set_section_grid(
+        #     grid=one_fault_model.grid,
+        #     section_dict={'section_SW-NE': ([250, 250], [1750, 1750], [100, 100]),
+        #                   'section_NW-SE': ([250, 1750], [1750, 250], [100, 100])}
+        # )
+        # 
+        # gp.set_topography_from_random(
+        #     grid=one_fault_model.grid,
+        #     fractal_dimension=1.2,
+        #     d_z=np.array([600, 2000]),
+        #     topography_resolution=np.array([60, 60])
+        # )
+
+        gp.compute_model(one_fault_model)
+        return one_fault_model
+
+    def test_plot_2d_solutions_default(self, one_fault_model_topo_solution_octrees):
+        print(one_fault_model_topo_solution_octrees.structural_frame)
+        _: Plot2D = gpv.plot_2d(one_fault_model_topo_solution_octrees)
