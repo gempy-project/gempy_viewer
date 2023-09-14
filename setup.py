ae2089f7752ee14357a6934986c8fc9eed7638a3
MigueldelaVarga <miguel@terranigma-solutions.com>
2023-09-14 08:22:50 +0200

    - [META] Update gempy_viewer setup.py

--- setup.py
+++ setup.py
@@ -2,19 +2,28 @@
 
 from setuptools import setup, find_packages
 
+version = "2023.1.0b1"
+
+
+def read_requirements(file_name):
+    with open(file_name, "r", encoding="utf-8") as f:
+        return [line.strip() for line in f.readlines()]
+
+
 setup(
     name='gempy_viewer',
-    version='0.1',
+    version=version,
     packages=find_packages(),
     url='',
     license='EUPL',
     author='Miguel de la Varga', 
-    author_email='',
+    author_email="miguel@terranigma-solutions.com",
     description='Viewer for the geological modeling package GemPy',
-    install_requires=['gempy>=2.2.9',
-                      'numpy',
-                      'matplotlib'
-                      ],
+    install_requires=read_requirements("requirements.txt"),
+    extras_require={
+        "opt": read_requirements("optional_requirements.txt"),
+        "dev": read_requirements("dev-requirements.txt"),
+    },
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Science/Research',
@@ -22,6 +31,5 @@ setup(
         'Topic :: Scientific/Engineering :: GIS',
         'Programming Language :: Python :: 3.10'
     ],
-    python_requires='>=3.10',
-    include_package_data=True,
+    python_requires='>=3.10'
 )
