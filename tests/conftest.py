511980fd2db2e8b193df4764ebac05a83fc9d8db
MigueldelaVarga <miguel@terranigma-solutions.com>
2023-10-11 10:49:37 +0200

    - [META] Rename

--- tests/conftest.py
+++ tests/conftest.py
@@ -43,7 +43,7 @@ def _one_fault_model_generator():
         project_name='fault',
         extent=[0, 2000, 0, 2000, 0, 1000],
         resolution=[20, 5, 20],
-        number_octree_levels=4,
+        refinement=4,
         importer_helper=ImporterHelper(
             path_to_surface_points=path_to_data + "model5_surface_points.csv",
             path_to_orientations=path_to_data + "model5_orientations.csv",
