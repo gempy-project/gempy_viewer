9fd7bdd818aa954527a3a4da0cd264b01c2e8064
MigueldelaVarga <miguel@terranigma-solutions.com>
2024-04-24 15:00:35 +0200

    [BUG][ENH] Update attribute names and method order
    Array attribute names within "utils_api.py" and "le_api.py" were updated for case sensitivity issues. The "require_liquid_earth_api()" method call position in "_plot_LiquidEarth.py" was optimized for better efficiency and proper token retrieval.

--- gempy_viewer/API/_plot_LiquidEarth.py
+++ gempy_viewer/API/_plot_LiquidEarth.py
@@ -8,13 +8,14 @@ def plot_to_liquid_earth(geo_model: gempy.data.GeoModel, space_name: str,
                          file_name: str = "gempy_model", user_token: Optional[str] = None, grab_link=True):
     
     # if user_token is None Try to grab it from the environment
+    liquid_earth_api = require_liquid_earth_api()  # ! Order matters
+    
     if user_token is None:
         import os
         user_token = os.environ.get('LIQUID_EARTH_API_TOKEN')
         if user_token is None:
             raise ValueError("No user token provided and no token found in the environment")
     
-    liquid_earth_api = require_liquid_earth_api()
     result = liquid_earth_api.upload_mesh_to_existing_space(
         space_name=space_name,
         data=geo_model.solutions.raw_arrays.meshes_to_subsurface(),
